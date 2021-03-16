a27bc8813317d89b5533e76c22ec44d38008db53
Richard <richarddavidrv13@gmail.com>
2021-03-16 16:35:51 -0400

    Cambios menores

--- static/scriptsm.js
+++ static/scriptsm.js
@@ -1,3 +1,16 @@
-console.log("Hola mundo");
-console.log("Hola mundo");
-console.log("Hola mundo");
+document.addEventListener("DOMContentLoaded", function (){
+    switch(window.location.pathname){
+        case "/":
+            document.getElementById('home').classList.add('active');
+        break;
+        case "/about":
+            document.getElementById('about').classList.add('active');
+        break;
+        case "/login":
+            document.getElementById('login').classList.add('active');
+        break;
+        case "/signup":
+            document.getElementById('signup').classList.add('active');
+        break;
+    }
+})
